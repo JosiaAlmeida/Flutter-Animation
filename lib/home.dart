9cf2d5612c95c0abd7a16502e8fcac5054d3cd85
JosiaAlmeida <josiaalmeida0001@gmail.com>
2022-04-26 09:37:37 +0100

    Animação linear

---
+++ lib/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var size = 100.0;
+  var increase = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Center(
+      child: GestureDetector(
+        onTap: () {
+          increase = !increase;
+          setState(() {});
+        },
+        child: AnimatedContainer(
+          duration: Duration(seconds: 2),
+          width: increase ? 200 : size,
+          height: increase ? 200 : size,
+          color: Colors.red,
+        ),
+      ),
+    ));
+  }
+}

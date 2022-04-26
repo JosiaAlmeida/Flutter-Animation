fd4131d38146da1b91ddf18d691d4b1be482f58a
JosiaAlmeida <josiaalmeida0001@gmail.com>
2022-04-26 09:54:17 +0100

    Animações implicitas(AnimatedCrossFade)

--- lib/home.dart
+++ lib/home.dart
@@ -8,26 +8,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var size = 100.0;
-  var increase = false;
+  var size = 400.0;
+  var isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-        child: Center(
+    return Scaffold(
+        body: Center(
       child: GestureDetector(
         onTap: () {
-          increase = !increase;
+          isLoading = !isLoading;
           setState(() {});
         },
         child: AnimatedContainer(
-          //Gerar curva na animação
-          curve: Curves.bounceOut,
-          duration: Duration(seconds: 2),
-          width: increase ? 200 : size,
-          height: increase ? 200 : size,
-          color: Colors.red,
-        ),
+            //Gerar curva na animação
+            curve: Curves.easeOut,
+            duration: Duration(milliseconds: 800),
+            width: isLoading ? 80 : 400,
+            height: 80,
+            alignment: Alignment.center,
+            color: Colors.red,
+            child: AnimatedCrossFade(
+              crossFadeState: isLoading
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: Duration(seconds: 1),
+              firstChild: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircularProgressIndicator(),
+              ),
+              secondChild: Text(
+                "ENTRAR",
+                textAlign: TextAlign.center,
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            )),
       ),
     ));
   }
